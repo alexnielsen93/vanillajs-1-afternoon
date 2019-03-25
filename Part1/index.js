20f3d5bb722c46b5384fb7d7a19013621d714938
Alex Nielsen <alexnielsen@Alexs-MacBook-Pro.local>
2019-03-25 13:30:40 -0600

    first commit

---
+++ Part1/index.js
@@ -0,0 +1,10 @@
+console.log("working")
+
+function play(){
+
+  if (document.getElementById('player')=== 'X'){
+    document.getElementById('player') = 'O'
+
+  }
+  else {document.getElementById('player') = 'X'}
+}
